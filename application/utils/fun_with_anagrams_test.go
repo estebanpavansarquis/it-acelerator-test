3f815c07ea222e8b6af2e9bcea3dab817bed1c98
EstebanPS <estebanpavansarquis@gmail.com>
2021-03-21 03:00:32 -0300

    GetFunWithAnagramsInput test added
    Signed-off-by: EstebanPS <estebanpavansarquis@gmail.com>

--- application/utils/fun_with_anagrams_test.go
+++ application/utils/fun_with_anagrams_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
+func TestGetFunWithAnagramsInput(t *testing.T) {
+	testingTable := []struct {
+		title          string
+		input          []int
+		expectedResult []string
+	}{
+		{"Input 0", []int{0}, FunWithAnagramsInputs[0]},
+		{"Input 1", []int{1}, FunWithAnagramsInputs[1]},
+		{"Input param negative", []int{-1}, FunWithAnagramsInputs[0]},
+		{"Input param not available", []int{5}, FunWithAnagramsInputs[0]},
+		{"Input param nil", []int{}, FunWithAnagramsInputs[0]},
+		{"Many input params", []int{1, 2, 3, 4}, FunWithAnagramsInputs[1]},
+	}
+
+	for _, testCase := range testingTable {
+		result := GetFunWithAnagramsInput(testCase.input...)
+		if !equals(result, testCase.expectedResult) {
+			t.Errorf("Test case \"%v\" failed with input: %v, expected: %v; got: %v", testCase.title, testCase.input, testCase.expectedResult, result)
+		}
+	}
+}
+
 func TestFunWithAnagramsInput(t *testing.T) {
 	testingTable := []struct {
 		title          string
